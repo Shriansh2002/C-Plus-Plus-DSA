bb726feea7cc2754f14c92ea42ac498c3e71433a
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-10 22:10:08 +0530

    reverseArray.cpp

---
+++ reverseArray.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+void reverseArray(vector<int> &arr , int m)
+{
+	int s = m+1;
+    int e = arr.size()- 1;
+
+    while(s <= e){
+    	int temp;
+
+        temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+
+        s++;
+        e--;
+    }
+	
+}
